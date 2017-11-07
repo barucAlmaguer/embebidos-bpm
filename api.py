6d1be1bc495bd6418e8c1854333b332a58df86dc
baruc <baruc.almaguer@gmail.com>
2017-11-07 08:45:13 -0600

    añadida ruta con 1 integer a app de flask como ejemplo

--- api.py
+++ api.py
@@ -1,7 +1,26 @@
 from flask import Flask
 
+import os
+from urllib import parse
+import psycopg2
+
+parse.uses_netloc.append("postgres")
+url = parse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Embebidos BMP"
+
+@app.route('/post/<int:post_id>')
+def integercillo(post_id):
+    return "integercillo squared = {}".format(post_id**2)
